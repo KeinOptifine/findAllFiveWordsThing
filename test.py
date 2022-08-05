5d226569911a2ce387aca81b279a895971076ced
Nikolas Heise <pop538548@inf-titan07.ad.uni-konstanz.de>
2022-08-05 11:51:38 +0200

    made the changees more usable

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-
-
-def check_viable(letters: list[str], result: list[str], word: str):
-    if word in result:
-        return False
-
-    viable = True
-    for c in word:
-        if c not in letters:
-            viable = False
-
-    return viable
-
+from main import check_viable
 
 letters = ["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
                    "u", "v", "w", "x", "y", "z"]
